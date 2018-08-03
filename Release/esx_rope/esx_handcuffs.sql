82e1d406f9cf4457e53d3f74c119ef880a62955c
Viktor <35453587+LifeGoal@users.noreply.github.com>
2018-08-04 00:41:57 +0200

    Delete esx_handcuffs.sql

--- Release/esx_rope/esx_handcuffs.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES (NULL, 'rope', 'Rope', -1, 0, 1);
-INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES (NULL, 'handcuffs', 'handcuffs', -1, 0, 1);
