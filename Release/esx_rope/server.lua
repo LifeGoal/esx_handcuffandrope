22f8f4b5755fd2f14f0e91e0529f9c12dba826ed
Viktor <35453587+LifeGoal@users.noreply.github.com>
2018-08-03 22:21:36 +0200

    Update server.lua

--- Release/esx_rope/server.lua
+++ Release/esx_rope/server.lua
@@ -2,17 +2,34 @@ ESX               = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-----
 ESX.RegisterUsableItem('rope', function(source)
 	local xPlayer = ESX.GetPlayerFromId(source)
 
 	TriggerClientEvent('esx_rope:ropecheck', source)
 end)
 
-----
-
 RegisterServerEvent('esx_rope:unlocking')
 AddEventHandler('esx_rope:unlocking', function(source)
   TriggerClientEvent('esx_rope:unlockingcuffs', source)
 end)
----
+
+ESX.RegisterUsableItem('handcuffs', function(source)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	TriggerClientEvent('esx_handcuffs:cuffcheck', source)
+end)
+
+RegisterServerEvent('esx_handcuffs:cuffing')
+AddEventHandler('esx_handcuffs:cuffing', function(source)
+  TriggerClientEvent('esx_handcuffs:cuff', source)
+end)
+
+ESX.RegisterUsableItem('key', function(source)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	xPlayer.removeInventoryItem('key', 1)
+	TriggerClientEvent('esx_handcuffs:unlockingcuffs', source)
+end)
+
+RegisterServerEvent('esx_handcuffs:unlocking')
+AddEventHandler('esx_handcuffs:unlocking', function(source)
+  TriggerClientEvent('esx_handcuffs:unlockingcuffs', source)
+end)
